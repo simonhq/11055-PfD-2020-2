b0d16fd1803f5887b861083903bf54e8eefb0507
ClaireMcA <claire.mcauliffe@outlook.com>
2020-10-21 16:39:29 +1100

    movie_database

--- project2/u3201052_u3212739/Project_2.js
+++ project2/u3201052_u3212739/Project_2.js
@@ -3,18 +3,6 @@
 Author: Zanya Nadelle Bendebel & Claire McAuliffe
 Date Created: 14/10/2020
 */
-function displayBooks(){
-    var myBooks = document.getElementsByClassName("header");
-    myBooks[0].innerHTML = "<h1>This is a Test!</h1>";
-    console.log(myBooks);
-}
-
-
-
-
-
-
-
 
 
 
@@ -25,9 +13,28 @@ pageQuantity = isbnlist.length;
 fetchDetails()
 
 async function fetchDetails(){
-    url = "https://openlibrary.org/api/books?bibkeys=ISBN:"+ isbnlist +"&format=json&jscmd=data";
-    const resp = await fetch(url);
-    const jres = await resp.json();
-    console.log(jres);
-    displayBooks();
+    var bookURL = "https://openlibrary.org/api/books?bibkeys=ISBN:"+ isbnlist +"&format=json&jscmd=data";
+    const respBook = await fetch(bookURL);
+    const jresBook = await respBook.json();
+    console.log(jresBook);
+    return jresBook
+
+    movieURL = "https://api.themoviedb.org/3/search/movie?api_key=58ab3248432c80d6ffd196da87c220a3&query=" + "Tales+from+EarthSea"
+    const respMovie = await fetch(movieURL);
+    const jresMovie = await respMovie.json();
+    console.log(jresMovie)
+    // displayBooks();
+    // document.write(jresBook[isbnlist[1]].title);
 }
+
+
+listNumber = 0
+
+function displayBooks(){
+    var displayTitle = document.getElementsByClassName("bookTitle");
+    displayTitle[listNumber].innerHTML = "<h1>" + jresBook[isbnlist[listnumber+1]].title + "</h1>";
+}
+
+
+
+displayBooks()

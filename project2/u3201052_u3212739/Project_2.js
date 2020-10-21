f19369c8243a0895e3ead5c437e55734bf0f4bad
ClaireMcA <claire.mcauliffe@outlook.com>
2020-10-22 10:37:37 +1100

    plugging_data

--- project2/u3201052_u3212739/Project_2.js
+++ project2/u3201052_u3212739/Project_2.js
@@ -3,11 +3,20 @@
 Author: Zanya Nadelle Bendebel & Claire McAuliffe
 Date Created: 14/10/2020
 */
+var isbnLength;
+
+// var pageNum = 0
 
 
+var pageNum = 0;
+function changePage(){
+    pageNum ++;
+    console.log(pageNum);
+}
 
 
-pageQuantity = isbnlist.length;
+// pageQty = isbnlist.length;
+// console.log(pageQty)
 
 
 fetchDetails()
@@ -17,24 +26,47 @@ async function fetchDetails(){
     const respBook = await fetch(bookURL);
     const jresBook = await respBook.json();
     console.log(jresBook);
-    return jresBook
 
-    movieURL = "https://api.themoviedb.org/3/search/movie?api_key=58ab3248432c80d6ffd196da87c220a3&query=" + "Tales+from+EarthSea"
-    const respMovie = await fetch(movieURL);
-    const jresMovie = await respMovie.json();
-    console.log(jresMovie)
-    // displayBooks();
-    // document.write(jresBook[isbnlist[1]].title);
+
+    // movieURL = "https://api.themoviedb.org/3/search/movie?api_key=58ab3248432c80d6ffd196da87c220a3&query=" + "Tales+from+EarthSea"
+    // const respMovie = await fetch(movieURL);
+    // const jresMovie = await respMovie.json();
+    // console.log(jresMovie)
+    
+    displayBook()
+
+    function displayBook(){
+        if (isbnlist[pageNum].toString().length == 10) {        // Check if ISBN number is 10 digits
+            displayBookCover("bookCover", isbnlist[pageNum]);
+            displayBookAttribute("bookTitle", jresBook["ISBN:" + isbnlist[pageNum]].title);
+            displayBookAttribute("bookSubtitle", jresBook["ISBN:" + isbnlist[pageNum]].subtitle);
+            displayBookAttribute("bookAuthor", jresBook["ISBN:" + isbnlist[pageNum]].authors[0].name);
+            displayBookAttribute("bookPublisher", jresBook["ISBN:" + isbnlist[pageNum]].publishers[0].name);
+            displayBookAttribute("bookPublishDate", jresBook["ISBN:" + isbnlist[pageNum]].publish_date);
+        } else {                                                // ISBN number is 13 digits
+            displayBookCover("bookCover", isbnlist[pageNum]);
+            displayBookAttribute("bookTitle", jresBook[isbnlist[pageNum]].title);
+            displayBookAttribute("bookAuthor", jresBook[isbnlist[pageNum]].authors[0].name);
+            displayBookAttribute("bookPublisher", jresBook[isbnlist[pageNum]].publishers[0].name);
+            displayBookAttribute("bookPublishDate", jresBook[isbnlist[pageNum]].publish_date);
+        }
+    }
+
+}
+
+// displayBooks()
+
+function displayBookAttribute(className, classVariable){
+    var displayAttribute = document.getElementsByClassName(className);
+    displayAttribute[0].innerHTML = "<p>" + classVariable + "</p>";
+}
+function displayBookCover(className,imgISBN){
+    var displayAttribute = document.getElementsByClassName(className);
+    displayAttribute[0].innerHTML = "<img src= https://covers.openlibrary.org/b/isbn/" + imgISBN + "-M.jpg >";
 }
 
 
-listNumber = 0
-
-function displayBooks(){
-    var displayTitle = document.getElementsByClassName("bookTitle");
-    displayTitle[listNumber].innerHTML = "<h1>" + jresBook[isbnlist[listnumber+1]].title + "</h1>";
-}
+// document.write(jresBook[isbnlist[1]].title);
 
 
 
-displayBooks()

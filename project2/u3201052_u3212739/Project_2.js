0fc268423427c4d2c4f443dcdc2efe4ece39f8c1
ClaireMcA <claire.mcauliffe@outlook.com>
2020-10-24 23:15:26 +1100

    save_with_value_field

--- project2/u3201052_u3212739/Project_2.js
+++ project2/u3201052_u3212739/Project_2.js
@@ -10,20 +10,41 @@ var jresBook;
 var isbnListKey = [];
 
 
+
 function onLoad(){
-    document.getElementById('nextButton').onclick = function changePage(){
-        pageNum ++;
-        console.log(pageNum);
-        displayBook();
-        window.scrollTo(0, 0);
-    }
-    document.getElementById('backButton').onclick = function changePage(){
-        pageNum --;
-        console.log(pageNum);
-        displayBook();
-        window.scrollTo(0, 0);
-    }
     expandISBN();
+    if (document.getElementById("bookListTitle")){
+        document.getElementById('nextButton').onclick = function changePage(){
+            pageNum ++;
+            console.log(pageNum);
+            displayBook();
+            window.scrollTo(0, 0);
+        }
+        document.getElementById('backButton').onclick = function changePage(){
+            pageNum --;
+            console.log(pageNum);
+            displayBook();
+            window.scrollTo(0, 0);
+        }
+        document.getElementById('deleteButton').onclick = function deleteISBN(){
+            isbnlist.splice(pageNum,1);
+            isbnListKey.splice(pageNum,1);
+            displayBook();
+            window.scrollTo(0, 0);
+        }
+        fetchDetails()
+    }
+    var newISBN = document.getElementById("addISBN");
+    if (newISBN){
+        newISBN.addEventListener('change', (event) => {
+            newISBN = document.getElementById("addISBN");
+            alert(newISBN.value);
+            isbnlist.unshift(newISBN.value);
+            
+        });
+    }
+
+    
 }
 window.onload = onLoad;
 
@@ -34,7 +55,7 @@ window.onload = onLoad;
 pageQty = isbnlist.length;
 // console.log(pageQty)
 
-fetchDetails()
+
 async function fetchDetails(){
     var bookURL = "https://openlibrary.org/api/books?bibkeys=ISBN:"+ isbnlist +"&format=json&jscmd=data";
     const respBook = await fetch(bookURL);
@@ -62,13 +83,20 @@ function expandISBN(){
     }
 }
 
-function displayButton(){
-    let displayNext = document.getElementsByClassName("nextButton");
-    displayNext[0].style.display = "block"
-    let displayBack = document.getElementsByClassName("backButton");
-    displayBack[0].style.display = "block"
+function displayButton(buttonName){
+    let displayButton = document.getElementsByClassName(buttonName);
+    displayButton[0].style.display = "block"
 }
-
+function hideButtons(){
+    let hideHome = document.getElementsByClassName("homeButton");
+    hideHome[0].style.display = "none"
+    let hideDelete = document.getElementsByClassName("deleteButton");
+    hideDelete[0].style.display = "none"
+    let hideNext = document.getElementsByClassName("nextButton");
+    hideNext[0].style.display = "none"
+    let hideBack = document.getElementsByClassName("backButton");
+    hideBack[0].style.display = "none"
+}
 function displayBookAttribute(className, classVariable){
     let displayAttribute = document.getElementsByClassName(className);
     displayAttribute[0].style.display = "block"
@@ -80,12 +108,12 @@ function displayBookCover(className,imgISBN){
     displayAttribute[0].innerHTML = "<img src= https://covers.openlibrary.org/b/isbn/" + imgISBN + "-M.jpg >";
 }
 function hideBookAttribute(className){
-    let displayAttribute = document.getElementsByClassName(className);
-    displayAttribute[0].style.display = "none"
+    let hideAttribute = document.getElementsByClassName(className);
+    hideAttribute[0].style.display = "none"
 }
 function hideBookCover(className){
-    let displayAttribute = document.getElementsByClassName(className);
-        displayAttribute[0].style.display = "none"
+    let hideAttribute = document.getElementsByClassName(className);
+        hideAttribute[0].style.display = "none"
 }
 
 // jresBook[isbnListKey[pageNum]] === isbnlist[pageNum]
@@ -97,50 +125,36 @@ function displayBook(){
         hideBookAttribute("bookAuthor");
         hideBookAttribute("bookPublisher");
         hideBookAttribute("bookPublishDate");
-        hideBookAttribute("nextButton");
+        hideButtons();
+        displayButton("backButton");
+        displayButton("homeButton");
+    } else if (!(isbnListKey[pageNum] in jresBook)){
+        hideBookCover("bookCover");
+        displayBookAttribute("bookTitle", "This book is not available. Please check the ISBN and try again.");
+        displayBookAttribute("bookAuthor", "");
+        displayBookAttribute("bookPublisher", "");
+        displayBookAttribute("bookPublishDate", "");
+        hideButtons();
+        displayButton("nextButton");
+        displayButton("backButton");
+        displayButton("deleteButton");
+    } else if(pageNum == 0){
+        displayBookCover("bookCover", isbnlist[pageNum]);
+        displayBookAttribute("bookTitle", jresBook[isbnListKey[pageNum]].title);
+        displayBookAttribute("bookAuthor", jresBook[isbnListKey[pageNum]].authors[0].name);
+        displayBookAttribute("bookPublisher", jresBook[isbnListKey[pageNum]].publishers[0].name);
+        displayBookAttribute("bookPublishDate", jresBook[isbnListKey[pageNum]].publish_date);
+        hideButtons();
+        displayButton("nextButton");
     } else {
-        if (!(isbnListKey[pageNum] in jresBook)){
-            hideBookCover("bookCover");
-            displayBookAttribute("bookTitle", "This book is not available. Please check the ISBN and try again.");
-            displayBookAttribute("bookAuthor", "");
-            displayBookAttribute("bookPublisher", "");
-            displayBookAttribute("bookPublishDate", "");
-            displayButton();
-        } else if(pageNum == 0){
-            if (isbnlist[pageNum].toString().length === 10) {        // Check if ISBN number is 10 digits
-                isbnListKey[pageNum] = "ISBN:" + isbnlist[pageNum];
-                displayBookCover("bookCover", isbnlist[pageNum]);
-                displayBookAttribute("bookTitle", jresBook["ISBN:" + isbnlist[pageNum]].title);
-                displayBookAttribute("bookAuthor", jresBook["ISBN:" + isbnlist[pageNum]].authors[0].name);
-                displayBookAttribute("bookPublisher", jresBook["ISBN:" + isbnlist[pageNum]].publishers[0].name);
-                displayBookAttribute("bookPublishDate", jresBook["ISBN:" + isbnlist[pageNum]].publish_date);
-                hideBookAttribute("backButton");
-            } else {                                                // ISBN number is 13 digits
-                isbnListKey[pageNum] = isbnlist[pageNum];
-                displayBookCover("bookCover", isbnlist[pageNum]);
-                displayBookAttribute("bookTitle", jresBook[isbnlist[pageNum]].title);
-                displayBookAttribute("bookAuthor", jresBook[isbnlist[pageNum]].authors[0].name);
-                displayBookAttribute("bookPublisher", jresBook[isbnlist[pageNum]].publishers[0].name);
-                displayBookAttribute("bookPublishDate", jresBook[isbnlist[pageNum]].publish_date);
-                hideBookAttribute("backButton");
-            }
-        } else {
-            if (isbnlist[pageNum].toString().length === 10) {        // Check if ISBN number is 10 digits
-                displayBookCover("bookCover", isbnlist[pageNum]);
-                displayBookAttribute("bookTitle", jresBook["ISBN:" + isbnlist[pageNum]].title);
-                displayBookAttribute("bookAuthor", jresBook["ISBN:" + isbnlist[pageNum]].authors[0].name);
-                displayBookAttribute("bookPublisher", jresBook["ISBN:" + isbnlist[pageNum]].publishers[0].name);
-                displayBookAttribute("bookPublishDate", jresBook["ISBN:" + isbnlist[pageNum]].publish_date);
-                displayButton();
-            } else {                                                // ISBN number is 13 digits
-                displayBookCover("bookCover", isbnlist[pageNum]);
-                displayBookAttribute("bookTitle", jresBook[isbnlist[pageNum]].title);
-                displayBookAttribute("bookAuthor", jresBook[isbnlist[pageNum]].authors[0].name);
-                displayBookAttribute("bookPublisher", jresBook[isbnlist[pageNum]].publishers[0].name);
-                displayBookAttribute("bookPublishDate", jresBook[isbnlist[pageNum]].publish_date);
-                displayButton();
-            }
-        }
+        displayBookCover("bookCover", isbnlist[pageNum]);
+        displayBookAttribute("bookTitle", jresBook[isbnListKey[pageNum]].title);
+        displayBookAttribute("bookAuthor", jresBook[isbnListKey[pageNum]].authors[0].name);
+        displayBookAttribute("bookPublisher", jresBook[isbnListKey[pageNum]].publishers[0].name);
+        displayBookAttribute("bookPublishDate", jresBook[isbnListKey[pageNum]].publish_date);
+        hideButtons();
+        displayButton("nextButton");
+        displayButton("backButton");
     }
 }
 

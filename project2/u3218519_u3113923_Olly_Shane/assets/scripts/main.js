3d829b15f3dbe4128e0cf44ec4ec76e467c30ea9
shane-ducksbury-uc <u3113923@uni.canberra.edu.au>
2020-10-27 13:40:41 +1100

    Olly and Shane Project 2 Submission

---
+++ project2/u3218519_u3113923_Olly_Shane/assets/scripts/main.js
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//  main.js                                                                 //
+//  Runs the program to populate books on The Book Repository Site          //
+//  Website by Olly Hills u3218519 & Shane Ducksbury u3113923               //
+//                                                                          //
+//  Last Edited on 24 October 2020                                          //
+//                                                                          //
+//  Changelog                                                               //
+//  Create the main.js file.                                                //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+
+// Specify the main function should call importPopulateBooks from app.js using the isbnlist
+function main () {
+    importPopulateBooks(isbnlist);
+}
+
+// call the main function.
+main();
